0dc905de40bff0206ca896a0edd13506995b96d9
mxtthewbarnes <matthew.m.barnes@wsu.edu>
2024-12-03 17:14:10 -0800

    Update MeteorShower.py

--- MeteorShower.py
+++ MeteorShower.py
@@ -62,79 +62,73 @@ def collision_check (met_pos, plyr_pos, plyr_siz, met_siz):
 
     
 def main():
-    '''
-    Initialize pygame and pygame parameters.  Note that both player and meteors
-    are square.  Thus, player_dim and met_dim are the height and width of the
-    player and meteors, respectively.  Each line of code is commented.
-    '''
-    pyg.init()                # initialize pygame
+    pyg.init()               
 
-    width = 800               # set width of game screen in pixels
-    height = 600              # set height of game screen in pixels
+    width = 800               
+    height = 600              
 
-    red = (255,0,0)           # rgb color of player
-    yellow = (244,208,63)     # rgb color of meteors
-    background = (0,0,156)    # rgb color of sky (midnight blue)
+    red = (255,0,0)           
+    yellow = (244,208,63)     
+    background = (0,0,156)    
 
-    player_dim = 50           # player size in pixels
-    player_pos = [width/2, height-2*player_dim]  # initial location of player
-                                                 # at bottom middle; height
-                                                 # never changes
+    player_dim = 50           
+    player_pos = [width/2, height-2*player_dim]  
+                                                 
 
-    met_dim = 20              # meteor size in pixels
-    met_list = []             # initialize list of two-element lists
-                              # giving x and y meteor positions
+    met_dim = 20             
+    met_list = []             
+                              
 
-    screen = pyg.display.set_mode((width, height)) # initialize game screen
+    screen = pyg.display.set_mode((width, height)) 
 
-    game_over = False         # initialize game_over; game played until
-                              # game_over is True, i.e., when collision
-                              # is detected
+    game_over = False         
+                              
+                             
 
-    score = 0                 # initialize score
+    score = 0                 
 
-    clock = pyg.time.Clock()  # initialize clock to track time
+    clock = pyg.time.Clock()  
 
-    my_font = pyg.font.SysFont("monospace", 35) # initialize system font
+    my_font = pyg.font.SysFont("monospace", 35) 
 
-    while not game_over:                       # play until game_over == True
-        for event in pyg.event.get():          # loop through events in queue
-            if event.type == pyg.KEYDOWN:      # checks for key press
-                x = player_pos[0]              # assign current x position
-                y = player_pos[1]              # assign current y position
-                if event.key == pyg.K_LEFT:    # checks if left arrow;
-                    x -= player_dim            # if true, moves player left
-                elif event.key == pyg.K_RIGHT: # checks if right arrow;
-                    x += player_dim            # else moves player right
-                player_pos = [x, y]            # reset player position
+    while not game_over:                       
+        for event in pyg.event.get():          
+            if event.type == pyg.KEYDOWN:     
+                x = player_pos[0]             
+                y = player_pos[1]              
+                if event.key == pyg.K_LEFT:    
+                    x -= player_dim            
+                elif event.key == pyg.K_RIGHT: 
+                    x += player_dim            
+                player_pos = [x, y]            
             
-        screen.fill(background)                # refresh screen bg color
-        drop_meteors(met_list, met_dim, width) # read PA prompt
-        speed = set_speed(score)               # read PA prompt
+        screen.fill(background)                
+        drop_meteors(met_list, met_dim, width)
+        speed = set_speed(score)               
         score = update_meteor_position(met_list, height, score, speed)
-                                               # read PA prompt
-        text = "Score: " + str(score)              # create score text
-        label = my_font.render(text, 1, yellow)    # render text into label
-        screen.blit(label, (width-250, height-40)) # blit label to screen at
-                                                   # given position; for our 
-                                                   # purposes, just thi
-                                                   # blit to mean draw
-        draw_meteors(met_list, met_dim, screen, yellow) # self-explanatory;
-                                                        # read PA prompt
+                                               
+        text = "Score: " + str(score)              
+        label = my_font.render(text, 1, yellow)    
+        screen.blit(label, (width-250, height-40)) 
+                                                  
+                                                   
+                                                   
+        draw_meteors(met_list, met_dim, screen, yellow) 
+                                                        
 
-        pyg.draw.rect(screen, red, (player_pos[0], player_pos[1], player_dim, player_dim))                                        # draw player
+        pyg.draw.rect(screen, red, (player_pos[0], player_pos[1], player_dim, player_dim))                                       
 #draw player
         if collision_check(met_list, player_pos, player_dim, met_dim):
-            game_over = True                       # read PA prompt
+            game_over = True                       
     
-        clock.tick(30)                             # set frame rate to control
-                                                   # frames per second (~30); 
-                                                   # slows down game
+        clock.tick(30)                             
+                                                   
+                                                   
 
-        pyg.display.update()                       # update screen characters
+        pyg.display.update()                      
     # Outside while-loop now.
-    print('Final score:', score)                   # final score
-    pyg.quit()                                     # leave pygame
+    print('Final score:', score)                   
+    pyg.quit()                                    
 
         
 main()
